32ff2baaf57b7d207327bc53d8bb507318e656ab
Martin Obadal <martin.obadal@blogic.cz>
2020-02-26 08:10:05 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    Začátek práce na forms
    
    # This is the commit message #2:
    
    Export inputu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TooltipDirective } from './directives/tooltip/tooltip.directive';
 import { TooltipService } from './services/tooltip.service';
+import { InputTextComponent } from './components/inputs/input-text.component';
 
 @NgModule({
 	imports: [BrowserModule, FormsModule, ReactiveFormsModule, MaterialModule],
-	declarations: [TooltipDirective],
-	exports: [TooltipDirective, TooltipService],
+	declarations: [TooltipDirective, InputTextComponent],
+	exports: [TooltipDirective, TooltipService, InputTextComponent],
 })
 export class SharedModule {}
