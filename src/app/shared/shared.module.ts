bcb221580a96a5a8a8d40a5646f8c9521a31f233
Martin Obadal <martin.obadal@blogic.cz>
2020-04-25 12:58:48 +0200

    Opravy

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IMaskModule } from 'angular-imask';
 import { TooltipDirective } from './directives/tooltip/tooltip.directive';
-import { TooltipService } from './services/tooltip.service';
 import { InputTextComponent } from './components/inputs/input-text.component';
 import { TooltipComponent } from './components/tooltip/tooltip.component';
 import { InputTextWithMaskComponent } from './components/inputs/input-text-with-mask.component';
@@ -31,7 +30,6 @@ import { IconComponent } from './components/icons/icon.component';
 		InputTextWithMaskComponent,
 		TooltipComponent,
 		TooltipDirective,
-		TooltipService,
 	],
 })
 export class SharedModule {}
